7d6c221de49cf3437b3972c10065f1897e69d18b
Gelzen Crisanto <cgelzen@gmail.com>
2024-05-11 22:13:46 -0700

    added spawnBounds and stacking restriction

--- Assets/Scripts/ObjectDragging.cs
+++ Assets/Scripts/ObjectDragging.cs
@@ -7,6 +7,11 @@ public class ObjectDragging : MonoBehaviour
 {
     public Tilemap tilemap;
     public GameObject draggedObject;
+    public float xLowBound = -8.5f;
+    public float xHighBound = -3.5f;
+    public float yLowBound = -2.5f;
+    public float yHighBound = 3.5f;
+    public bool[ , ] isOccupied = new bool[6, 7];
     // Start is called before the first frame update
     void Start()
     {
@@ -32,9 +37,20 @@ public class ObjectDragging : MonoBehaviour
         if (Input.GetMouseButtonUp(0) && draggedObject)
         {
             Vector3Int cellPosition = tilemap.LocalToCell(mousePosition);
-            draggedObject.transform.localPosition = tilemap.GetCellCenterLocal(cellPosition) + Vector3.forward;
-            draggedObject = null;
+            Vector3 cellCenter = tilemap.GetCellCenterLocal(cellPosition);
+            int row = (int)(cellCenter.x + 8.5);
+            int col = (int)(cellCenter.y + 2.5);
+            if (cellCenter.x >= xLowBound && cellCenter.x <= xHighBound && cellCenter.y >= yLowBound && cellCenter.y <= yHighBound && !isOccupied[row, col])
+            {
+                draggedObject.transform.localPosition = cellCenter + Vector3.forward;
+                isOccupied[row, col] = true;
+            }
+            else
+            {
+                Destroy(draggedObject);
+            }
 
+            draggedObject = null;
         }
         if (draggedObject)
         {
